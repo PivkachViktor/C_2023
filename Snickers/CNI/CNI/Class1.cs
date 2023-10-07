018fd5cb17164d8d86e8f757d02a719b002f1ee2
PivkachViktor <126353363+PivkachViktor@users.noreply.github.com>
2023-10-07 18:35:07 +0300

    Add files via upload

---
+++ Snickers/CNI/CNI/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections;
+
+namespace CNI
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            // Додавання елементів у стек (приклад)
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            int product = CalculateProductOfOddElementsAtEvenPositions(stack);
+
+            if (product == 0)
+            {
+                Console.WriteLine("Непарних елементів, що стоять на парних позиціях в стеку немає");
+            }
+            else
+            {
+                Console.WriteLine("Добуток непарних елементів на парних позиціях: " + product);
+            }
+        }
+
+        static int CalculateProductOfOddElementsAtEvenPositions(Stack<int> stack)
+        {
+            int product = 1;
+            int position = 0;
+
+            foreach (int element in stack)
+            {
+                if (position % 2 == 0 && element % 2 != 0)
+                {
+                    product *= element;
+                }
+
+                position++;
+            }
+
+            return product;
+
+        }
+    }
+}
---
+++ Snickers/CNI/CNI/Завдання_2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNI
+{
+    internal class Завдання_2
+    {
+        static void Main(string[] args)
+        {
+            // Створення черги та додавання елементів (назв штатів США)
+            Queue<string> statesQueue = new Queue<string>();
+            statesQueue.Enqueue("California");
+            statesQueue.Enqueue("Texas");
+            statesQueue.Enqueue("New York");
+            statesQueue.Enqueue("Florida");
+            statesQueue.Enqueue("Illinois");
+
+            // Виведення всіх елементів на консоль в прямому порядку
+            Console.WriteLine("Елементи в прямому порядку:");
+            foreach (string state in statesQueue)
+            {
+                Console.WriteLine(state);
+            }
+
+            // Виведення всіх елементів на консоль в зворотному порядку
+            Console.WriteLine("\nЕлементи в зворотному порядку:");
+            string[] statesArray = statesQueue.ToArray();
+            for (int i = statesArray.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(statesArray[i]);
+            }
+
+            // Виведення кількості елементів у колекції
+            Console.WriteLine("\nКількість елементів у колекції: " + statesQueue.Count);
+
+            // Очищення колекції
+            statesQueue.Clear();
+            Console.WriteLine("\nКолекція очищена.");
+
+            // Перевірка кількості елементів після очищення
+            Console.WriteLine("Кількість елементів у колекції після очищення: " + statesQueue.Count);
+        }
+    }
+}
