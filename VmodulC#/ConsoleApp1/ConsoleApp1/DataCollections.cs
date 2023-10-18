0aec78a55d17b7aebe7e902b80ff6ffeb97a0717
PivkachViktor <126353363+PivkachViktor@users.noreply.github.com>
2023-10-18 09:31:20 +0300

    Add files via upload

---
+++ VmodulC#/ConsoleApp1/ConsoleApp1/DataCollections.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+
+public class Hotel
+{
+    public List<HotelRoom> HotelRooms { get; set; }
+    public List<Tenant> Tenants { get; set; }
+
+    public double GetAverageStayDurationInRoomLastYear(int roomNumber)
+    {
+        DateTime lastYear = DateTime.Now.AddYears(-1);
+
+        var tenantsInRoomLastYear = Tenants
+            .Where(tenant => tenant.RoomNumber == roomNumber && tenant.CheckInDate >= lastYear);
+
+        if (tenantsInRoomLastYear.Any())
+        {
+            double totalStayDuration = tenantsInRoomLastYear
+                .Sum(tenant => (tenant.CheckOutDate - tenant.CheckInDate).TotalDays);
+
+            return totalStayDuration / tenantsInRoomLastYear.Count();
+        }
+        else
+        {
+            return 0; // Якщо немає жильців в цій кімнаті за останній рік
+        }
+    }
+
+    public IEnumerable<int> GetFloorsWithMinRooms()
+    {
+        var minRoomCount = HotelRooms
+            .GroupBy(room => room.Floor)
+            .Min(group => group.Count());
+
+        var floorsWithMinRooms = HotelRooms
+            .Where(room => room.Floor == HotelRooms
+                .GroupBy(r => r.Floor)
+                .Where(g => g.Count() == minRoomCount)
+                .Select(g => g.Key)
+                .First())
+            .Select(room => room.Floor)
+            .Distinct();
+
+        return floorsWithMinRooms;
+    }
+
+    public void SaveHotelRoomsToXml(string fileName)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(List<HotelRoom>));
+
+        using (FileStream fileStream = new FileStream(fileName, FileMode.Create))
+        {
+            serializer.Serialize(fileStream, HotelRooms);
+        }
+    }
+
+}
+
+public struct HotelRoom
+{
+    public int RoomNumber { get; set; }
+    public int Floor { get; set; }
+    public int Capacity { get; set; }
+    public decimal DailyRate { get; set; }
+}
+
+public struct Tenant
+{
+    public string LastName { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public int RoomNumber { get; set; }
+    public DateTime CheckOutDate { get; set; }
+}
---
+++ VmodulC#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        
+        Hotel hotel = new Hotel
+        {
+            HotelRooms = new List<HotelRoom>
+            {
+                new HotelRoom { RoomNumber = 111, Floor = 1, Capacity = 4, DailyRate = 100 },
+                new HotelRoom { RoomNumber = 112, Floor = 1, Capacity = 3, DailyRate = 120 },
+                new HotelRoom { RoomNumber = 113, Floor = 2, Capacity = 2, DailyRate = 90 },
+                new HotelRoom { RoomNumber = 114, Floor = 2, Capacity = 1, DailyRate = 130 },
+                new HotelRoom { RoomNumber = 114, Floor = 3, Capacity = 1, DailyRate = 130 },
+            },
+            Tenants = new List<Tenant>
+            {
+                new Tenant { LastName = "Smith", CheckInDate = new DateTime(2023, 1, 15), RoomNumber = 111, CheckOutDate = new DateTime(2023, 1, 20) },
+                new Tenant { LastName = "Johnson", CheckInDate = new DateTime(2023, 2, 10), RoomNumber = 112, CheckOutDate = new DateTime(2023, 2, 20) },
+                new Tenant { LastName = "Williams", CheckInDate = new DateTime(2023, 3, 5), RoomNumber = 113, CheckOutDate = new DateTime(2023, 3, 10) },
+                new Tenant { LastName = "Brown", CheckInDate = new DateTime(2023, 4, 8), RoomNumber = 114, CheckOutDate = new DateTime(2023, 4, 15) },
+                new Tenant { LastName = "Brown", CheckInDate = new DateTime(2023, 4, 8), RoomNumber = 114, CheckOutDate = new DateTime(2023, 4, 15) },
+            }
+        };
+
+        int roomNumberToCheck = 114; 
+
+        double averageStayDuration = hotel.GetAverageStayDurationInRoomLastYear(roomNumberToCheck);
+
+        Console.WriteLine($"Середня тривалість проживання у кімнаті {roomNumberToCheck}: {averageStayDuration} днів");
+
+        
+        var floors = hotel.GetFloorsWithMinRooms();
+
+        Console.WriteLine("Поверхи з найменшою кількістю кімнат:");
+        foreach (var floor in floors)
+        {
+            Console.WriteLine($"Поверх {floor}");
+        }
+
+        
+        string xmlFileName = "HotelRooms.xml";
+        hotel.SaveHotelRoomsToXml(xmlFileName);
+
+        Console.WriteLine($"Дані про кімнати готелю збережено у файлі {xmlFileName}");
+
+    }
+}
